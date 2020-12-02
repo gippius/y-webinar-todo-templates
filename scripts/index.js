74eb7e90c626fb53af29b1acb27f179ec569c214
Constantine Gippius <konstantin.gippius@t-systems.com>
2020-12-02 18:23:36 +0300

    feat: initial commit

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+
+const elList = document.querySelector('.todo__list');
+const elAddButton = document.querySelector('.button_add');
+const elInput = document.querySelector('.input');
+const elTemplate = document.querySelector('.template');
+
+const TODO_LIST = [
+    { title: 'Съесть печеньку 🍪' },
+    { title: 'Изучить React ⚛️' },
+    { title: 'Полить цветы 🌸' },
+    { title: 'Сделать свой сайт 👨‍💻' },
+    { title: 'Погулять в парке 🌳' },
+];
