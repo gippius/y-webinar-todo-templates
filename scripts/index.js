7fb3727c9c69bc361c73a35188d3585455d9d0ff
Constantine Gippius <konstantin.gippius@t-systems.com>
2020-12-03 14:03:08 +0300

    feat: code optizimation

--- scripts/index.js
+++ scripts/index.js
@@ -10,40 +10,49 @@ const inputElement = document.querySelector('.input');
 const templateElement = document.querySelector('#card-template');
 
 function renderList() {
-    let listHTML = '';
-    listHTML = TODO_LIST.map(composeItem);
+    const listHTML = TODO_LIST.map(composeItem);
     listContainerElement.append(...listHTML)
 }
 
-function composeItem(item){
+function composeItem(item) {
     const itemElement = templateElement.cloneNode(true).content;
     const headerElement = itemElement.querySelector('.card__title');
     headerElement.textContent = item.title;
-    
-    itemElement.querySelector('.button_remove').addEventListener('click', (event) => {
-        const targetItemElement = event.target;
-        console.log(targetItemElement.closest('.card'))
-        targetItemElement.closest('.card').remove();
-    })
-    
-    itemElement.querySelector('.button_duplicate').addEventListener('click', (event)=>{
-        const targetElement = event.target;
-        const targetItemText = targetElement.closest('.card').querySelector('.card__title').innerText;
-        const newItemElement = composeItem({ title: targetItemText })
-        listContainerElement.prepend(newItemElement)
-    })
-    
+    itemElement.querySelector('.button_remove').addEventListener('click', removeItem)
+    itemElement.querySelector('.button_duplicate').addEventListener('click', duplicateItem)
     return itemElement;
 }
 
-function bindEventListeners() {
-    const addButtonElement = document.querySelector('.button_add');
+function composeItem({ title }) {
+    const itemElement = templateElement.cloneNode(true).content;
+    const headerElement = itemElement.querySelector('.card__title');
+    headerElement.textContent = title;
+    itemElement.querySelector('.button_remove').addEventListener('click', removeItem)
+    itemElement.querySelector('.button_duplicate').addEventListener('click', duplicateItem)
+    return itemElement;
+}
 
-    addButtonElement.addEventListener('click', function () {
-        const newItem =  composeItem({ title: inputElement.value })
-        listContainerElement.prepend(newItem)
-    })
+function removeItem(event) {
+    const targetItemElement = event.target;
+    targetItemElement.closest('.card').remove();
+}
+
+function duplicateItem(event) {
+    const targetElement = event.target;
+    const targetItemText = targetElement.closest('.card').querySelector('.card__title').innerText;
+    const newItemElement = composeItem({ title: targetItemText })
+    listContainerElement.prepend(newItemElement)
+}
+
+function addItem(){
+    const newItem = composeItem({ title: inputElement.value })
+    listContainerElement.prepend(newItem)
+}
+
+function bindAddItemEventListener() {
+    const addButtonElement = document.querySelector('.button_add');
+    addButtonElement.addEventListener('click', addItem)
 }
 
 renderList();
-bindEventListeners();
+bindAddItemEventListener();
