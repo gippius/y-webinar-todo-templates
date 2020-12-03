0b8cd7c6595d4ff3a5ed29ef5715df139e9beaf2
Constantine Gippius <konstantin.gippius@t-systems.com>
2020-12-03 20:44:53 +0300

    feat: composeItemHTML function

--- scripts/index.js
+++ scripts/index.js
@@ -11,17 +11,19 @@ const listContainerElement = document.querySelector('.todo__list');
 function renderList() {
     let newHTML = '';
 
-    newHTML = TODO_LIST.map(function (item) {
-        return `<li class="todo__item card">
+    newHTML = TODO_LIST.map(composeItemHTML);
+
+    listContainerElement.insertAdjacentHTML('afterbegin', newHTML);
+}
+
+function composeItemHTML(item){
+    return `<li class="todo__item card">
                     <h2 class="card__title">${item.title}</h2>
                     <div class="card__actions">
                         <button type="button" class="button button_duplicate"></button>
                         <button type="button" class="button button_remove"></button>
                     </div>
                 </li>`  
-    }).join('');
-
-    listContainerElement.insertAdjacentHTML('afterbegin', newHTML);
 }
 
 renderList();
