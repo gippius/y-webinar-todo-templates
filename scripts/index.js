6b582c50e4c45f18bddc442eb63ba1874a856a69
Constantine Gippius <konstantin.gippius@t-systems.com>
2020-12-03 20:53:40 +0300

    feat: add new item

--- scripts/index.js
+++ scripts/index.js
@@ -7,23 +7,36 @@ const TODO_LIST = [
 ];
 
 const listContainerElement = document.querySelector('.todo__list');
+const inputElement = document.querySelector('input');
 
 function renderList() {
     let newHTML = '';
 
-    newHTML = TODO_LIST.map(composeItemHTML);
+    newHTML = TODO_LIST.map(composeItemHTML).join('');
 
     listContainerElement.insertAdjacentHTML('afterbegin', newHTML);
 }
 
-function composeItemHTML(item){
+function composeItemHTML(item) {
     return `<li class="todo__item card">
                     <h2 class="card__title">${item.title}</h2>
                     <div class="card__actions">
                         <button type="button" class="button button_duplicate"></button>
                         <button type="button" class="button button_remove"></button>
                     </div>
-                </li>`  
+                </li>`
+}
+
+function bindAddItemListener() {
+    const addButtonElement = document.querySelector('.button_add');
+    addButtonElement.addEventListener('click', addNewItem)
+}
+
+function addNewItem(){
+        const inputText = inputElement.value;
+        const newItemHTML = composeItemHTML({ title: inputText })
+        listContainerElement.insertAdjacentHTML('afterbegin', newItemHTML);
 }
 
 renderList();
+bindAddItemListener();
