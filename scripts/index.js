d87c964598abc438b322c140d2a65835504e9330
Constantine Gippius <konstantin.gippius@t-systems.com>
2020-12-03 11:26:41 +0300

    feat: dynamic list rendering

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,3 @@
-
-const elList = document.querySelector('.todo__list');
-const elAddButton = document.querySelector('.button_add');
-const elInput = document.querySelector('.input');
-const elTemplate = document.querySelector('.template');
-
 const TODO_LIST = [
     { title: 'Съесть печеньку 🍪' },
     { title: 'Изучить React ⚛️' },
@@ -11,3 +5,23 @@ const TODO_LIST = [
     { title: 'Сделать свой сайт 👨‍💻' },
     { title: 'Погулять в парке 🌳' },
 ];
+
+function renderList() {
+    const listContainerElement = document.querySelector('.todo__list');
+    let listHTML = '';
+
+    listHTML = TODO_LIST.map(item => {
+        return `<li class="todo__item card">
+                <h2 class="card__title">${item.title}</h2>
+                <div class="card__actions">
+                  <button type="button" class="button button_duplicate"></button>
+                  <button type="button" class="button button_remove"></button>
+                </div>
+              </li>`
+    }).join('');
+
+    listContainerElement.insertAdjacentHTML('afterbegin', listHTML)
+
+}
+
+renderList();
